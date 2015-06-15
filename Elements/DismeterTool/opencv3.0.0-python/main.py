94f703ad94dd4d61b34d06eb1e9475d0f46d2e14
himingway <badppg@qq.com>
2015-06-15 16:54:48 +0800

    fix bugs

--- Elements/DismeterTool/opencv3.0.0-python/main.py
+++ Elements/DismeterTool/opencv3.0.0-python/main.py
@@ -9,13 +9,13 @@ cv2.namedWindow('image')
 cv2.createTrackbar('Threshold','image',140,255,nothing)
 cv2.createTrackbar('DBlur','image',8,15,nothing)
 while(1):
-
     k=cv2.waitKey(1)&0xFF
     if k==27:
-        break
+        cv2.destroyAllWindows()
     Threshold=cv2.getTrackbarPos('Threshold','image')
     DBlur=cv2.getTrackbarPos('DBlur','image')
     src = cv2.imread("1.bmp", 1)
+    cv2.imshow("image",src)
     img = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
     a,img2 = cv2.threshold(img,Threshold,255,cv2.THRESH_BINARY)
     img3 = cv2.medianBlur(img2,(2*DBlur+1))
@@ -32,5 +32,4 @@ while(1):
         cv2.putText(src,"r= "+a+"um", (110,123), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
         cv2.putText(src,"R=150.00"+"um", (110,90), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,130,0),2)
         cv2.imshow("image",src)
-        cv2.imshow("image",src)
-cv2.destroyAllWindows()
+        cv2.imshow("image",src)   
